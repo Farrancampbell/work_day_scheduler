095ea85888cec31643965e159ed9cd85571d1959
Farrancampbell <farranerinandcrew@gmail.com>
2021-03-27 15:01:46 -0400

    Finished working with Juan my Tutor

---
+++ script.js
@@ -0,0 +1,36 @@
+var today = moment();
+
+console.log("today");
+$("#currentDay").text(today.format("MMM Do, YYYY, hh:mm:ss"));
+
+var hrs = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var currentHour = today.hours();
+for (var i = 0; i < hrs.length; i++) {
+  if (currentHour === hrs[i]) {
+    $("#" + hrs[i]).addClass("present");
+  } else if (currentHour > hrs[i]) {
+    $("#" + hrs[i]).addClass("past");
+  } else {
+    $("#" + hrs[i]).addClass("future");
+  }
+}
+$(".saveBtn").on("click", function () {
+  var hr = $(this).data("hr");
+  var description = $("#" + hr).val();
+  localStorage.setItem(hr, description);
+});
+for (var i = 0; i < hrs.length; i++) { 
+  $("#" + hrs[i]).val(localStorage.getItem(hrs[i])); 
+}
+
+var seconds = moment().format("ss");
+setInterval( function(){
+  console.log(seconds)
+  if (seconds >= 59 ){
+    seconds = 0
+    $("#currentDay").text(today.format("MMM Do, YYYY, hh:mm:ss"));
+  }
+  else {
+    seconds = seconds+1
+  }
+},1000) 
